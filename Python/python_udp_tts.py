026f7f1dfb73455069a4de78df22f5a84ea02e89
paupau <72361177+megakitten@users.noreply.github.com>
2022-09-14 16:29:40 -0600

    add header and osc return message on play finish

--- Python/python_udp_tts.py
+++ Python/python_udp_tts.py
@@ -1,6 +1,23 @@
-## Needs playsound installed
+## Listens to OSC port for a string.
+## On string receive, synthesize a
+## sound file and play it. 
+## Send 
+
+## -- mvpvh (Paulus)
+##    with borrowed code from:
+##      -->
+##      -->
+##      -->
+##      -->
+
+##################################
+##################################
+
+## RUN: pip install playsound
+##      before running this script
 
 import argparse
+from operator import truediv
 import random
 import time
 import math
@@ -28,20 +45,19 @@ def tts_string_handler(unused_addr, args, ttsString):
     except ValueError:
         pass
     print("Starting Synthesizer")
+    client.send_message("/Synthesizing", "True")
     now = datetime.now()
     fileName = "test_" + now.strftime("%m%d%Y_%H%M%S") + ".wav"
-    synthesizeCmd = "tts --text \"" + ttsString + ".\" --out_path " + fileName
-    synthResult = run(synthesizeCmd)
-    ##print(synthResult)
-
+    synthesize = "tts --text \"" + ttsString + ".\" --out_path " + fileName
+    synthResult = run(synthesize)
     print("Done Synthesizing")
-
-    client.send_message("/doneSynthesizing", "true")
+    client.send_message("/DoneSynthesizing", "True")
 
     # TO DO : add a check that the file is there 
     print("Playing new sound file")
-    playsound(fileName)
-    print("Played new sound file")
+    playsound(fileName, True) # blocking
+
+    client.send_message("/DoneSpeaking", "True")
 
 if __name__ == "__main__":
 #################
@@ -57,16 +73,13 @@ if __name__ == "__main__":
 
 #################
     # SERVER PART
-
     parser1 = argparse.ArgumentParser()
-    parser1.add_argument("--ip",
-                        default="127.0.0.1", help="The ip to listen on")
-    parser1.add_argument("--port",
-                        type=int, default=5005, help="The port to listen on")
+    parser1.add_argument("--ip", default="127.0.0.1", help="The ip to listen on")
+    parser1.add_argument("--port", type=int, default=5005, help="The port to listen on")
     args1 = parser1.parse_args()
 
     dispatcher = Dispatcher()
-    dispatcher.map("/tts", tts_string_handler, "Synthesizing String")
+    dispatcher.map("/tts", tts_string_handler, "Synthesizing speech...")
 
     server = osc_server.ThreadingOSCUDPServer(
         (args1.ip, args1.port), dispatcher)
