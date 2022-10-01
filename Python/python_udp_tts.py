400feffbef1ee78c88a9be5f066502641fbbc98d
paupau <72361177+megakitten@users.noreply.github.com>
2022-10-01 15:48:11 -0600

    make working version of python script

--- Python/python_udp_tts.py
+++ Python/python_udp_tts.py
@@ -14,6 +14,7 @@
 ##################################
 
 ## RUN: pip install playsound
+## RUN: pip install pythonosc
 ##      before running this script
 
 import argparse
@@ -45,19 +46,26 @@ def tts_string_handler(unused_addr, args, ttsString):
     except ValueError:
         pass
     print("Starting Synthesizer")
-    client.send_message("/Synthesizing", "True")
+    client.send_message("/Progress", "Synthesizing")
     now = datetime.now()
+    #fileName = "test_" + now.strftime("%m%d%Y_%H%M%S") + ".wav"
     fileName = "test_" + now.strftime("%m%d%Y_%H%M%S") + ".wav"
-    synthesize = "tts --text \"" + ttsString + ".\" --out_path " + fileName
+    synthesize = "tts --text \"" + ttsString + "\" --out_path C:/Users/megac/Github/TTSFromUnityOSC/Python/" + fileName
     synthResult = run(synthesize)
     print(synthResult)
     print("Done Synthesizing")
-    client.send_message("/DoneSynthesizing", "True")
+    client.send_message("/Progress", "Done Synthesizing")
+    # TO DO : add a check that the file is there
+    try:
+        print("Playing new sound file")
+        client.send_message("/Progress", "Start Speaking")
+        # playsound("C:/Users/megac/Github/test_10012022_151633.wav", True)  # blocking
+        playsound(fileName, True)  # blocking
+        client.send_message("/Progress", "Done Speaking")
+    except ValueError:
+        pass
+        client.send_message("/Progress", "Error Speaking")
 
-    # TO DO : add a check that the file is there 
-    print("Playing new sound file")
-    playsound(fileName, True) # blocking
-    client.send_message("/DoneSpeaking", "True")
 
 if __name__ == "__main__":
 #################
@@ -65,8 +73,8 @@ if __name__ == "__main__":
     parser2 = argparse.ArgumentParser()
     parser2.add_argument("--ip", default="127.0.0.1",
                         help="The ip of the OSC server")
-    parser2.add_argument("--port", type=int, default=8001,
-                        help="The port the OSC server is listening on")
+    parser2.add_argument("--port", type=int, default=8000,
+                        help="The port the OSC server to listen to")
     args2 = parser2.parse_args()
 
     client = udp_client.SimpleUDPClient(args2.ip, args2.port)
@@ -74,12 +82,12 @@ if __name__ == "__main__":
 #################
     # SERVER PART
     parser1 = argparse.ArgumentParser()
-    parser1.add_argument("--ip", default="127.0.0.1", help="The ip to listen on")
-    parser1.add_argument("--port", type=int, default=8000, help="The port to listen on")
+    parser1.add_argument("--ip", default="127.0.0.1", help="The ip to serve on")
+    parser1.add_argument("--port", type=int, default=8001, help="The port to serve on")
     args1 = parser1.parse_args()
 
     dispatcher = Dispatcher()
-    dispatcher.map("/user_input", tts_string_handler, "Synthesizing speech...")
+    dispatcher.map("/user_input", tts_string_handler, "Synthesizing Speech...")
 
     server = osc_server.ThreadingOSCUDPServer(
         (args1.ip, args1.port), dispatcher)
